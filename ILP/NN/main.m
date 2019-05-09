573f14e0e38ab1ce11aacdc63105dca37d59e96b
Yantong WANG <yantong.wang@kcl.ac.uk>
2019-05-09 18:35:45 +0100

    basic code structure of NN

---
+++ ILP/NN/main.m
@@ -0,0 +1,28 @@
+%%
+clear
+clc
+
+%% load training data and label
+
+
+
+
+%% construct neural network  layers
+
+
+
+
+
+%% training neural network
+
+
+
+
+
+
+%% load test data and label
+
+
+
+%% test trained neural network
+
