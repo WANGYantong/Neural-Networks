ce91c502a9ad93383dfbb9a1b72af23034d32aa5
Yantong Wang <yantong.wang@kcl.ac.uk>
2019-06-11 09:31:55 +0100

    add more training data

--- ILP/NN/main.m
+++ ILP/NN/main.m
@@ -8,11 +8,11 @@ labfile='../DataStore/imgLabels.mat';
 load(imgfile);
 load(labfile);
 
-imgDataTrain=imgData(:,:,:,1:8000);
-imgLabelsTrain=imgLabels(1:8000,:);
+imgDataTrain=imgData(:,:,:,1:9000);
+imgLabelsTrain=imgLabels(1:9000,:);
 
-imgDataTest=imgData(:,:,:,8001:10000);
-imgLabelsTest=imgLabels(8001:10000,:);
+imgDataTest=imgData(:,:,:,9001:10000);
+imgLabelsTest=imgLabels(9001:10000,:);
 
 %% construct neural network  layers
 layers = [
@@ -33,47 +33,47 @@ layers = [
     convolution2dLayer(3,64,'Padding',1)
     batchNormalizationLayer
     reluLayer
-	
-%     fullyConnectedLayer(6)
-%     softmaxLayer
-%     classificationLayer];
-    fullyConnectedLayer(10)
-    regressionLayer];
+    
+    fullyConnectedLayer(6)
+    softmaxLayer
+    classificationLayer];
+%     fullyConnectedLayer(10)
+%     regressionLayer];
 
 %% training neural network
 miniBatchSize = 1024;
 options = trainingOptions( 'sgdm',...  
-    'ExecutionEnvironment','cpu',...
+    'ExecutionEnvironment','auto',...
     'MiniBatchSize', miniBatchSize,...
     'MaxEpochs', 60, ...
      'InitialLearnRate',0.0001,...
     'Plots', 'training-progress');
 
-% net = cell(10,1);
-% 
-% parfor ii=1:10
-%     net{ii} = trainNetwork(imgDataTrain, categorical(imgLabelsTrain(:,ii)), layers, options);
-% end
-net=trainNetwork(imgDataTrain, imgLabelsTrain, layers, options);
+net = cell(10,1);
+
+for ii=1:10
+    net{ii} = trainNetwork(imgDataTrain, categorical(imgLabelsTrain(:,ii)), layers, options);
+end
+% net=trainNetwork(imgDataTrain, imgLabelsTrain, layers, options);
 
 %% test trained neural network
-% predLabelsTestMedium = cell(10,1);
-% for ii=1:10
-%     predLabelsTestMedium{ii} = net{ii}.classify(imgDataTest);
-% end
-% predLabelsTest=[predLabelsTestMedium{1:10}];
-predLabelsTest=net.predict(imgDataTest);
+predLabelsTestMedium = cell(10,1);
+for ii=1:10
+    predLabelsTestMedium{ii} = net{ii}.classify(imgDataTest);
+end
+predLabelsTest=[predLabelsTestMedium{1:10}];
+% predLabelsTest=net.predict(imgDataTest);
 
 counter=0;
 for jj=1:length(imgLabelsTest)
-%     if all(predLabelsTest(jj,:)==categorical(imgLabelsTest(jj,:)))
-    if all(round(predLabelsTest(jj,:))==imgLabelsTest(jj,:))
+    if all(predLabelsTest(jj,:)==categorical(imgLabelsTest(jj,:)))
+%     if all(round(predLabelsTest(jj,:))==imgLabelsTest(jj,:))
         counter=counter+1;
     end
 end
 accuracy_final = counter / length(imgLabelsTest);
 
-% accuracy=zeros(1,10);
-% for ii=1:10
-%     accuracy(ii) = sum(predLabelsTestMedium{ii} ==categorical(imgLabelsTest(:,ii))) / length(imgLabelsTest);
-% end
+accuracy=zeros(1,10);
+for ii=1:10
+    accuracy(ii) = sum(predLabelsTestMedium{ii} ==categorical(imgLabelsTest(:,ii))) / length(imgLabelsTest);
+end
